18203479f46e6daf292d8de3a0d48db5559b7485
muhammadahmadd <66084909+muhammadahmadd@users.noreply.github.com>
2022-04-13 16:50:56 +0500

    for mac users

--- src/settings.js
+++ src/settings.js
@@ -7,3 +7,5 @@ export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
 export const channelName = "Test";
+
+
